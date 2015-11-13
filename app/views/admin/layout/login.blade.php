fc7658d6ee4e1ca476f11c7a41fd728e88204e97
dxnm <2711sq@gmail.com>
2015-11-13 16:58:28 +0700

    fix login.blade.php

--- app/views/admin/layout/login.blade.php
+++ app/views/admin/layout/login.blade.php
@@ -8,7 +8,6 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.5 -->
   {{HTML::style('admin/bootstrap/css/bootstrap.min.css') }}
-    </head>
   <!-- Font Awesome -->
   {{HTML::style('admin/dist/css/font-awesome.min.css') }}
   <!-- Ionicons -->
