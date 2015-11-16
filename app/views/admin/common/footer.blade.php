9fc5a5b88306b7a260823d67a71fee0bde221acc
dxnm <2711sq@gmail.com>
2015-11-16 08:31:13 +0700

    fix name folder public/admin to public/adminlte

--- app/views/admin/common/footer.blade.php
+++ app/views/admin/common/footer.blade.php
@@ -1,21 +1,21 @@
 <!-- jQuery 2.1.4 -->
-{{ HTML::script('admin/plugins/jQuery/jQuery-2.1.4.min.js') }}
+{{ HTML::script('adminlte/plugins/jQuery/jQuery-2.1.4.min.js') }}
 <!-- jQuery UI 1.11.4 -->
-{{ HTML::script('admin/plugins/jQueryUI/jquery-ui.min.js') }}
+{{ HTML::script('adminlte/plugins/jQueryUI/jquery-ui.min.js') }}
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.5 -->
-{{ HTML::script('admin/bootstrap/js/bootstrap.min.js') }}
+{{ HTML::script('adminlte/bootstrap/js/bootstrap.min.js') }}
 <!-- daterangepicker -->
-{{ HTML::script('admin/plugins/daterangepicker/moment.min.js') }}
-{{ HTML::script('admin/plugins/daterangepicker/daterangepicker.js') }}
+{{ HTML::script('adminlte/plugins/daterangepicker/moment.min.js') }}
+{{ HTML::script('adminlte/plugins/daterangepicker/daterangepicker.js') }}
 <!-- datepicker -->
-{{ HTML::script('admin/plugins/datepicker/bootstrap-datepicker.js') }}
+{{ HTML::script('adminlte/plugins/datepicker/bootstrap-datepicker.js') }}
 <!-- Bootstrap WYSIHTML5 -->
-{{ HTML::script('admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}
+{{ HTML::script('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}
 <!-- FastClick -->
-{{ HTML::script('admin/plugins/fastclick/fastclick.js') }}
+{{ HTML::script('adminlte/plugins/fastclick/fastclick.js') }}
 <!-- AdminLTE App -->
-{{ HTML::script('admin/dist/js/app.min.js') }}
+{{ HTML::script('adminlte/dist/js/app.min.js') }}
