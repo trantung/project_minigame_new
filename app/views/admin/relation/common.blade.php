cec100ba61f2298bb35f54c23cc12abda5549bde
tuancuongth88 <tuancuongth88@gmail.com>
2015-11-21 11:53:33 +0700

    add more script

--- app/views/admin/relation/common.blade.php
+++ app/views/admin/relation/common.blade.php
@@ -34,6 +34,7 @@
 		});
 	}
 </script>
+
 <div class="row margin-bottom">
 	<div class="col-xs-12">
 		<a href="{{ action('RelationController@index') }} " class="btn btn-success">Danh sách Relation</a>
