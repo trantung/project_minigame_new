cd35270f7f939d515540c6d68ea36839557b610f
dxnm <2711sq@gmail.com>
2015-11-23 16:50:42 +0700

    fix model for slug

--- app/models/CategoryParent.php
+++ app/models/CategoryParent.php
@@ -3,13 +3,21 @@
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class CategoryParent extends Eloquent
+class CategoryParent extends Eloquent implements SluggableInterface
 {
 	use SoftDeletingTrait;
+    use SluggableTrait;
     protected $table = 'category_parents';
-    protected $fillable = ['name', 'position', 'description', 'weight_number'];
+    protected $fillable = ['name', 'position', 'description', 'weight_number', 'slug'];
     protected $dates = ['deleted_at'];
+
+    protected $sluggable = array(
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    );
 
     public function categoryparentrelations()
     {
--- app/models/TypeNew.php
+++ app/models/TypeNew.php
@@ -3,13 +3,21 @@
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class TypeNew extends Eloquent
+class TypeNew extends Eloquent implements SluggableInterface
 {
 	use SoftDeletingTrait;
+	use SluggableTrait;
     protected $table = 'type_news';
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
     protected $dates = ['deleted_at'];
+
+    protected $sluggable = array(
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    );
 
     public function news()
     {
