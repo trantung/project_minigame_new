f4d8c5c90e6e47d693d44c0793b3ce399ad3d2b6
dxnm <2711sq@gmail.com>
2015-12-02 10:29:58 +0700

    login google fix

--- app/controllers/site/GoogleController.php
+++ app/controllers/site/GoogleController.php
@@ -82,16 +82,60 @@ class GoogleController extends BaseController {
 		//
 	}
 
-	public function getSuccess()
+	public function logingoogle()
     {
-        return SocialLogin::checkLoginGoogle();
-    }
+        $code = Input::get( 'code' );
 
-    public function getLogout()
-    {
-        GoogleOAuth::logout();
-        return Redirect::to("/");
-    }
+        // get google service
+        $googleService = GoogleOAuth::consumer();
 
+        // check if code is valid
+
+        // if code is provided get user data and sign in
+        if ( !empty( $code ) ) {
+
+            // This was a callback request from google, get the token
+            $token = $googleService->requestAccessToken( $code );
+
+            // Send a request with it
+            $result = json_decode( $googleService->request( 'https://www.googleapis.com/oauth2/v1/userinfo' ), true );
+
+            // Check to see if user already exists
+            if($user = User::where('email', '=', $result['email'])->first())
+            {
+                Auth::user()->login($user);
+                // If user isn't activated redirect them
+                if ($user->status == 0)
+                {
+                    dd('ok');
+                }
+                $input = CommonSite::ipDeviceUser();
+            	$user->update($input);
+                return Redirect::action('SiteIndexController@index');
+            }
+            else
+            {
+                // Create new user waiting for approval
+                $input = CommonSite::inputRegister();
+	            $input['email'] = $result['email'];
+	            $input['google_id'] = $result['id'];
+	            $input['google_name'] = $result['name'];
+	        	$userId = User::create($input)->id;
+
+                return Redirect::action('SiteIndexController@index');
+                // return Redirect::back()->withErrors(['Your account have been created. It is awaiting activation by your manager']);
+            }
+
+
+        }
+        // if not ask for permission first
+        else {
+            // get googleService authorization
+            $url = $googleService->getAuthorizationUri();
+            // return to google login url
+            return Redirect::to( (string)$url );
+        }
+
+    }
 
 }
