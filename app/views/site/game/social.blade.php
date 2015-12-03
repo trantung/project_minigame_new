e5f3ee002769176eb7f990eb1f16716a065522ee
dxnm <2711sq@gmail.com>
2015-12-03 10:58:50 +0700

    fix social

--- app/views/site/game/social.blade.php
+++ app/views/site/game/social.blade.php
@@ -1,5 +1,5 @@
 <p class="social">
-	<div class="fb-like" data-share="true" data-show-faces="false" data-href="{{ Request::url() }}"></div>
+	<div class="fb-like" data-width="100" data-layout="button_count" data-share="true" data-show-faces="false" data-href="{{ Request::url() }}"></div>
 	<a href="https://plus.google.com/share?url={{ Request::url() }}"><img src="{{ url('/assets/images/shareGoogle.png') }}" width="80px" height="22px"></a>
 	<a href="{{ action('SiteFeedbackController@errorGame', array('id' => $id)) }}" class="report-error"><i class="fa fa-warning"></i> Báo lỗi</a>
 </p>
