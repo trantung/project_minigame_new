07b672044c4add99990b3fb1ae28cf65293fd7c9
trantung <trantunghn196@gmail.com>
2015-12-03 14:45:03 +0700

    fix

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'gamemini',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'abc123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
