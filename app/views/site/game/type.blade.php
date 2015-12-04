b0f61ca6947b391dcd6f91a28c2e827a507863e1
Tran Thanh Tung <tranthanhtung@Trans-MacBook-Pro.local>
2015-12-04 13:45:07 +0700

    fix

--- app/views/site/game/type.blade.php
+++ app/views/site/game/type.blade.php
@@ -5,58 +5,68 @@
 @stop
 
 @section('content')
+<h1>Game {{ $type->name }} hay nhất</h1>
 
-<div class="box">
-	<h1>{{ $type->name }}</h1>
-	<div class="row">
-		@foreach($games as $game)
-			<div class="col-xs-6 col-sm-3 col-md-2">
-				<div class="item">
-					<div class="item-image">
-						<a href="{{ CommonGame::getUrlGame($game->slug) }}">
-							<img src="{{ url(UPLOAD_GAME_AVATAR . '/' .  $game->image_url) }}" alt="{{ $game->name }}" />
-							<strong>{{ limit_text($game->name, TEXTLENGH) }}</strong>
-						</a>
-					</div>
-					<div class="item-play">
-						<a href="{{ CommonGame::getUrlGame($game->slug) }}"><span>{{ getZero($game->count_play) }} lượt chơi</span><i class="play"><img src="{{ url('/assets/images/play.png') }}"></i></a>
-					</div>
+<div id="demo">
+	<div id="owl-demo" class="owl-carousel">
+		@for($i =0; $i < $count ; $i ++)
+			<div class="box">
+				<div class="row">
+				<?php 
+					$listGame = $games->orderBy('count_play', 'desc')->take(PAGINATE_BOXGAME)->skip($i * PAGINATE_BOXGAME)->get();
+				?>
+					@foreach($listGame as $game)
+						<div class="col-xs-6 col-sm-3 col-md-2">
+							<div class="item">
+								<div class="item-image">
+									<a href="{{ CommonGame::getUrlGame($game->slug) }}">
+										<img src="{{ url(UPLOAD_GAME_AVATAR . '/' .  $game->image_url) }}" alt="{{ $game->name }}" />
+										<strong>{{ limit_text($game->name, TEXTLENGH) }}</strong>
+									</a>
+								</div>
+								<div class="item-play">
+									<a href="{{ CommonGame::getUrlGame($game->slug) }}"><span>{{ getZero($game->count_play) }} lượt chơi</span><i class="play"><img src="{{ url('/assets/images/play.png') }}"></i></a>
+								</div>
+							</div>
+						</div>
+					@endforeach
 				</div>
+				{{ $i+1 }}/{{ $count }}
 			</div>
-		@endforeach
+		@endfor
 	</div>
-
-	<div class="row">
-		<div class="col-xs-12">
-			<ul class="pagination">
-			{{ $games->appends(Request::except('page'))->links() }}
-			</ul>
-		</div>
-	</div>
-
 </div>
+  {{ HTML::script('assets/js/owl.carousel.js') }}
+  {{ HTML::style('assets/css/owl.carousel.css') }}
+  {{ HTML::style('assets/css/owl.theme.css') }}
+	<!-- Demo -->
+	<style>
+	#owl-demo .item img{
+		display: block;
+		width: 100%;
+		height: auto;
+	}
+	</style>
+	<script>
+	$(document).ready(function() {
+	  $("#owl-demo").owlCarousel({
+	  navigation : true,
+	  slideSpeed : 100,
+	  autoPlay: false,
+	  paginationSpeed : 100,
+	  pagination: false,
+	  singleItem : true,
+	  paginationNumbers:true
 
-@if($relationModel = CommonSite::getRelationModel($type->id, 'Type'))
-	<div class="box">
-		<h1>{{ $relationModel->name }}<a href="{{ $relationModel->slug }}" class="box-more">Xem thêm</a></h1>
-		<div class="row">
-			@foreach(CommonGame::boxGameByType($relationModel) as $game)
-				<div class="col-xs-6 col-sm-3 col-md-2">
-					<div class="item">
-						<div class="item-image">
-							<a href="{{ CommonGame::getUrlGame($game->slug) }}">
-								<img src="{{ url(UPLOAD_GAME_AVATAR . '/' .  $game->image_url) }}" alt="{{ $game->name }}" />
-								<strong>{{ $game->name }}</strong>
-							</a>
-						</div>
-						<div class="item-play">
-							<a href="{{ CommonGame::getUrlGame($game->slug) }}"><span>{{ $game->count_play }} lượt chơi</span><i class="play"><img src="{{ url('/assets/images/play.png') }}"></i></a>
-						</div>
-					</div>
-				</div>
-			@endforeach
-		</div>
-	</div>
-@endif
+	  // "singleItem:true" is a shortcut for:
+	  // items : 1, 
+	  // itemsDesktop : false,
+	  // itemsDesktopSmall : false,
+	  // itemsTablet: false,
+	  // itemsMobile : false
+
+	  });
+	});
+	</script>
 
 @stop
