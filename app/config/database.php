bb6558cad2134246a01cfab84da787603481d082
trantung <trantunghn196@gmail.com>
2015-12-09 15:31:42 +0700

    fix_database

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'gamemini',
 			'username'  => 'root',
-			'password'  => 'abc123',
+			'password'  => '@bcc0m@555',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
