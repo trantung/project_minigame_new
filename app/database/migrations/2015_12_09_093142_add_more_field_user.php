3b4c924c6077487dc31ee7c14abef66bf813dc91
Tran Thanh Tung <tranthanhtung@Trans-MacBook-Pro.local>
2015-12-09 09:35:27 +0700

    add image_url into user table

---
+++ app/database/migrations/2015_12_09_093142_add_more_field_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMoreFieldUser extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table) {
+            $table->string('image_url')->after('fullname')->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
--- app/models/User.php
+++ app/models/User.php
@@ -25,7 +25,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 	protected $fillable = array('user_name', 'email', 'password', 
-		'uid', 'uname', 'first_name', 'last_name', 
+		'uid', 'uname', 'first_name', 'last_name', 'image_url'
 		'fullname', 'status', 'ip', 'device', 'phone', 'google_id', 'google_name','fb_access_token');
     protected $dates = ['deleted_at'];
 
