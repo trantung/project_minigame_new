d9cded3304107d6a98cb64731f88a56ddd3b0d91
dxnm <2711sq@gmail.com>
2015-12-09 09:10:52 +0700

    fix

--- app/views/site/game/comment.blade.php
+++ app/views/site/game/comment.blade.php
@@ -2,14 +2,18 @@
 <div class="comment">
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs" role="tablist">
-		<li role="presentation" class="active"><a href="#comment1" aria-controls="comment1" role="tab" data-toggle="tab">choinhanh.vn</a></li>
-		<li role="presentation"><a href="#comment2" aria-controls="comment2" role="tab" data-toggle="tab">Facebook</a></li>
+		<li role="presentation" class="active"><a href="#comment2" aria-controls="comment2" role="tab" data-toggle="tab">Facebook</a></li>
+		<li role="presentation"><a href="#comment1" aria-controls="comment1" role="tab" data-toggle="tab">choinhanh.vn</a></li>
 	</ul>
 	@include('site.common.message_comment')
 	<!-- Tab panes -->
 	<div class="tab-content">
 
-		<div role="tabpanel" class="tab-pane active" id="comment1">
+		<div role="tabpanel" class="tab-pane active" id="comment2">
+			<div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="5"></div>
+		</div>
+
+		<div role="tabpanel" class="tab-pane" id="comment1">
 		{{ Form::open(array('action' => array('SiteCommentController@update', $game->id), 'method' => 'PUT')) }}
 		<!-- comment cho game -->
 		@if(Auth::user()->check() && !(Auth::user()->get()->uid))
@@ -52,8 +56,6 @@
 		</div>
 		{{ Form::close() }}
 		</div>
-		<div role="tabpanel" class="tab-pane" id="comment2">
-			<div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="5"></div>
-		</div>
+		
 	</div>
 </div>
