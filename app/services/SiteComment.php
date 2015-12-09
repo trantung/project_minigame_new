d81bb20e763e39e9c25a435d2c3b981005ccf4f4
dxnm <2711sq@gmail.com>
2015-12-09 13:21:47 +0700

    fix

--- app/services/SiteComment.php
+++ app/services/SiteComment.php
@@ -3,7 +3,7 @@ class SiteComment {
 
 	public static function getCommentGame($game)
 	{
-    	$inputComment = Comment::where('model_id', $game->id)->where('status', ACTIVE)->orderBy('id', 'desc')->paginate(PAGE_COMMENT);
+    	$inputComment = Comment::where('model_id', $game->id)->where('status', ACTIVE)->orderBy('id', 'desc')->get();
     	return $inputComment;
 	}
 }
