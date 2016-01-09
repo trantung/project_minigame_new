0a9c4978e40edaf9dac0c5db842f512cd13817a3
dxnm <2711sq@gmail.com>
2016-01-09 16:57:23 +0700

    fix menu + delete errors log

--- public/script/menu.js
+++ public/script/menu.js
@@ -1,4 +1,9 @@
-var ajax = new XMLHttpRequest();
-ajax.open("GET", "/script/menu.html", false);
-ajax.send();
-document.body.innerHTML += ajax.responseText;
+function is_touch_device() {
+return !!('ontouchstart' in window);
+}
+if(is_touch_device()) {
+	var ajax = new XMLHttpRequest();
+	ajax.open("GET", "/script/menu.html", false);
+	ajax.send();
+	document.body.innerHTML += ajax.responseText;
+}
