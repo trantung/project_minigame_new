fb6bc2f1f83386b638b68b0aca17b81893cfc6a9
trantung <trantunghn196@gmail.com>
2016-02-17 10:38:19 +0700

    fix

--- app/controllers/site/SiteNewsController.php
+++ app/controllers/site/SiteNewsController.php
@@ -44,16 +44,32 @@ class SiteNewsController extends SiteController {
 	 * @param  int  $id
 	 * @return Response
 	 */
+	// public function show($slug)
+	// {
+	// 	$now = date('Y-m-d');
+	// 	$inputNew = AdminNew::findBySlug($slug);
+	// 	$input['count_view'] = getZero($inputNew->count_view) + 1;
+	// 	CommonNormal::update($inputNew->id, $input, 'AdminNew');
+	// 	$inputRelated = AdminNew::where('type_new_id', $inputNew->type_new_id)->where('start_date', '<=', $now)->orderBy(DB::raw('RAND()'))->limit(PAGINATE_RELATED)->get();
+	// 	return View::make('site.News.showNews')->with(compact('inputNew', 'inputRelated'));
+	// }
+
 	public function show($slug)
 	{
 		$now = date('Y-m-d');
-		$inputNew = AdminNew::findBySlug($slug);
-		$input['count_view'] = getZero($inputNew->count_view) + 1;
-		CommonNormal::update($inputNew->id, $input, 'AdminNew');
-		$inputRelated = AdminNew::where('type_new_id', $inputNew->type_new_id)->where('start_date', '<=', $now)->orderBy(DB::raw('RAND()'))->limit(PAGINATE_RELATED)->get();
-		return View::make('site.News.showNews')->with(compact('inputNew', 'inputRelated'));
+		// $newTypeId = TypeNew::findBySlug($slug)->id;
+		$newType = TypeNew::findBySlug($slug);
+		$news = AdminNew::where('type_new_id', $newType->id)->get();
+		return View::make('site.News.showType')->with(compact('newType', 'news'));
 	}
-
+	public function showNew($slugType, $slugNew)
+	{
+		$now = date('Y-m-d');
+		// $newTypeId = TypeNew::findBySlug($slug)->id;
+		$newType = TypeNew::findBySlug($slug);
+		$news = AdminNew::where('type_new_id', $newType->id)->get();
+		return View::make('site.News.showType')->with(compact('newType', 'news'));
+	}
 	/**
 	 * Show the form for editing the specified resource.
 	 *
