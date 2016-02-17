7937d713cb64a2815ceb778ae5e49b95aa3f9450
trantung <trantunghn196@gmail.com>
2016-02-17 10:39:37 +0700

    fix_routes

--- app/routes.php
+++ app/routes.php
@@ -181,7 +181,8 @@ Route::post('/send-error-game', array('uses' => 'SiteFeedbackController@sendErro
 
 Route::get('/tim-kiem-game', array('uses' => 'SearchGameController@index', 'as' => 'searchGame'));
 
-Route::get('/tin-tuc/{slug}', array('uses' => 'SiteNewsController@show', 'as' =>'showNews'));
+Route::get('/tin-tuc/{slug}', array('uses' => 'SiteNewsController@show', 'as' =>'showType'));
+Route::get('/tin-tuc/{slugType}/{slugNew}', array('uses' => 'SiteNewsController@showNew', 'as' =>'showNew'));
 Route::get('/tin-tuc', array('uses' => 'SiteNewsController@index', 'as' => 'listNews'));
 
 Route::put('/comment/{id}', array('uses' => 'SiteCommentController@update'));
