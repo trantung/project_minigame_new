80a9a845f42714c7cc4299e4279b907658e6d095
trantung <trantunghn196@gmail.com>
2016-02-22 08:02:59 +0700

    fix

--- app/views/site/common/type.blade.php
+++ app/views/site/common/type.blade.php
@@ -1,7 +1,7 @@
 @if(Request::segment(1) == MINI_GAME_SLUG)
 	<ul class="nav-type">
-		<li><a href="#">Ban trai</a></li>
-		<li><a href="#">Be gai</a></li>
-		<li><a href="#">Ban sung</a></li>
+	@foreach(Type::all() as $value)
+		<li><a href="{{ url('/' . $value->slug) }}">{{ ($value->name) }}</a></li>
+	@endforeach
 	</ul>
 @endif
