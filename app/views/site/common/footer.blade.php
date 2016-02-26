36771d9fb737eb38ff9a00306c2a9dd2e083f57c
trantung <trantunghn196@gmail.com>
2016-02-26 09:01:36 +0700

    fix_footer

--- app/views/site/common/footer.blade.php
+++ app/views/site/common/footer.blade.php
@@ -5,9 +5,15 @@
 	  <li><a href="{{ action('SiteFeedbackController@policy') }}" {{ checkActive('chinh-sach') }}>Chính sách</a></li>
 	</ul>
 	<div class="copyright">
-	  	<p>&copy; Copyright 2014-2015 kienthuc.net.vn, all rights reserved.</p>
-		{{-- <p>Cơ quan chủ quản: Công ty Cổ phần truyền thông ABC</p>
-		<p>Địa chỉ: P501, Tầng 5, Tòa nhà văn phòng, Số 5B/55, Huỳnh Thúc Kháng, Phường Láng Hạ, Quận Đống Đa, Hà Nội</p>
-		<p>Tel: (84-4) 3.775.4334 - Fax: (84-4) 3512 1804</p> --}}
+	  	Chuyên trang của Báo điện tử Kiến Thức
+		Giấy phép: Số 07/ GP-CBC Cục Báo chí - Bộ TTTT. Cấp ngày: 31 tháng 08 năm 2015.
+		<br/>
+		Tổng biên tập: Nguyễn Minh Quang.
+		Tòa soạn: Số 465B Hoàng Hoa Thám, Quận Ba Đình, Hà Nội.
+		<br/> 
+		Hotline: 096 532 77 56.
+		Điện thoại: 04.6269.3999 máy lẻ 189. Fax: 04.32474786 Email: tkts@kienthuc.net.vn
+		<br/>
+		Liên hệ quảng cáo: Ms Hằng - Điện thoại: 0948 849 848.
 	</div>
 </div>
