db1da5691385c21d97039c06c6e678bd6dd1e123
tuancuongth88 <tuancuongth88@gmail.com>
2016-03-02 16:45:53 +0700

    update reporter

--- app/views/admin/newsreporter/create.blade.php
+++ app/views/admin/newsreporter/create.blade.php
@@ -22,6 +22,23 @@
 							</div>
 						</div>
 					</div>
+					<div class="form-group">
+						<label>Loại tin</label>
+						<div class="row">
+							<div class="col-sm-6">
+							   {{  Form::select('type', [INACTIVE => 'Tin thường', ACTIVE => 'Tin ảnh'], null, array('class' => 'form-control')) }}
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label>Hình tin ảnh</label>
+						<div class="row">
+							<div class="col-sm-6">
+							   {{ Form::file('image_urls[]', array('id' => 'image_url', 'multiple' => true)) }}
+							</div>
+						</div>
+
+					</div>
 					<!-- <div class="form-group">
 						<label for="start_date">Ngày xuất bản</label>
 						<div class="row">
