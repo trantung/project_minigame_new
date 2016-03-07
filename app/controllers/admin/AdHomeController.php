f61f9abe6494f3df7497ccd082c91afd58c1d1c1
dxnm <2711sq@gmail.com>
2016-03-07 10:12:32 +0700

    fix

---
+++ app/controllers/admin/AdHomeController.php
@@ -0,0 +1,96 @@
+<?php
+
+class AdHomeController extends AdminController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$advertise = Advertise::whereNull('model_name')
+								->whereNull('model_id')
+								->get();
+		return View::make('admin.ad.desktop.index')->with(compact('advertise'));
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('admin.ad.desktop.create');
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$input = Input::except('_token');
+		$id = Advertise::create($input)->id;
+		return Redirect::action('AdHomeController@index')->with('message', 'tạo mới thành công');
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		$advertise = Advertise::find($id);
+		return View::make('admin.ad.desktop.edit')->with(compact('advertise'));
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		$input = Input::except('_token');
+		$advertise = Advertise::find($id);
+		$advertise->update($input);
+		return Redirect::action('AdHomeController@index')->with('message', 'Thay đổi thành công');
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		Advertise::find($id)->delete();
+		return Redirect::action('AdHomeController@index')->with('message', 'Xoá thành công');
+	}
+
+
+}
