cd6108fd91330bc8494e8d488c1394a0bfb120c2
dxnm <2711sq@gmail.com>
2016-03-07 08:38:44 +0700

    fix

---
+++ app/views/admin/ad/create.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.layout.default')
+
+@section('title')
+{{ $title='Thêm mới quảng cáo trang chủ' }}
+@stop
+
+@section('content')
+
+<div class="row margin-bottom">
+	<div class="col-xs-12">
+		<a href="{{ action('AdvertiseController@index') }} " class="btn btn-success">Danh sách quảng cáo trang chủ</a>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-xs-12">
+		<div class="box box-primary">
+			<!-- form start -->
+			{{ Form::open(array('action' => array('AdvertiseController@store'), 'files' => true)) }}
+			<div class="box-body">
+				<div class="form-group">
+					<label for="name">Vị trí</label>
+					<div class="row">
+						<div class="col-sm-6">
+						   {{ Form::select('position', ['']) }}
+						</div>
+					</div>
+				</div>
+				<div class="form-group ad-adsense">
+					<label for="name">Adsense</label>
+					<div class="row">
+						<div class="col-sm-6">
+							{{ Form::textarea('adsense', null, textParentCategory('code adsense')) }}
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="name">Status</label>
+					<div class="row">
+						<div class="col-sm-6">
+						   {{ Form::select('status', [DISABLED => 'Ẩn', ENABLED => 'Hiển thị']) }}
+						</div>
+					</div>
+				</div>
+			  <div class="box-footer">
+				{{ Form::submit('Lưu lại', array('class' => 'btn btn-primary')) }}
+			  </div>
+			{{ Form::close() }}
+		  </div>
+		  <!-- /.box -->
+		</div>
+	</div>
+</div>
+
+@include('admin.adverties.script')
+
+@stop
