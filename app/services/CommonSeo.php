d5469d6225a62f0001e853f3b79554f210c7ef94
dxnm <2711sq@gmail.com>
2016-03-11 14:43:01 +0700

    fix

--- app/services/CommonSeo.php
+++ app/services/CommonSeo.php
@@ -51,41 +51,7 @@ class CommonSeo
 		if(Input::hasFile($imageUrl)){
 			$file = Input::file($imageUrl);
 			$filename = $file->getClientOriginalName();
-
-			//start code
-			$image = imagecreatefromjpeg($file);
-			// $filename = 'images/cropped_whatever.jpg';
-			$thumb_width = 600;
-			$thumb_height = 450;
-			$width = imagesx($image);
-			$height = imagesy($image);
-			$original_aspect = $width / $height;
-			$thumb_aspect = $thumb_width / $thumb_height;
-			if ( $original_aspect >= $thumb_aspect )
-			{
-			   // If image is wider than thumbnail (in aspect ratio sense)
-			   $new_height = $thumb_height;
-			   $new_width = $width / ($height / $thumb_height);
-			}
-			else
-			{
-			   // If the thumbnail is wider than the image
-			   $new_width = $thumb_width;
-			   $new_height = $height / ($width / $thumb_width);
-			}
-			$thumb = imagecreatetruecolor( $thumb_width, $thumb_height );
-			// Resize and crop
-			imagecopyresampled($thumb,
-			                   $image,
-			                   0 - ($new_width - $thumb_width) / 2, // Center the image horizontally
-			                   0 - ($new_height - $thumb_height) / 2, // Center the image vertically
-			                   0, 0,
-			                   $new_width, $new_height,
-			                   $width, $height);
-			imagejpeg($thumb, $filename, 80);
-			//end code
-
-			$uploadSuccess = $file->move($destinationPath, $thumb);
+			$uploadSuccess = $file->move($destinationPath, $filename);
 			return $filename;
 		}
 		if ($imageSeo) {
