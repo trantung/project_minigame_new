35b8b72bb4aa81ffcfc471a82cfa325bcfee4dfe
dxnm <2711sq@gmail.com>
2016-03-16 09:09:34 +0700

    fix

--- app/views/site/News/slideNews.blade.php
+++ app/views/site/News/slideNews.blade.php
@@ -30,12 +30,7 @@
 				<div class="col-sm-9 col-sm-push-3">
 					<div class="detail">
 						<strong>{{ $inputNew->sapo_text . $inputNew->sapo }}</strong>
-						<div class="clearfix"></div>
-						@if(getDevice() == COMPUTER)
-							@include('site.common.ads', array('adPosition' => POSITION_SAPO, 'model_name' => 'AdminNew', 'model_id' => $inputNew->id))
-						@else
-							@include('site.common.ads', array('adPosition' => POSITION_MOBILE_SAPO, 'model_name' => 'AdminNew', 'model_id' => $inputNew->id))
-						@endif
+
 						<div class="clearfix"></div>
 						@if($inputNewSlide)
 
