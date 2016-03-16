2cbb6c9a26db0e63b6179a561b0dc88ec10f0990
dxnm <2711sq@gmail.com>
2016-03-16 16:28:52 +0700

    fix

--- app/views/site/News/slideNews.blade.php
+++ app/views/site/News/slideNews.blade.php
@@ -83,7 +83,7 @@
 					@include('site.News.hotNews')
 
 					@if(getDevice() == COMPUTER)
-						@include('site.common.ads', array('adPosition' => POSITION_NEWS_DETAIL_LEFT, 'model_name' => 'AdminNew', 'model_id' => $inputNew->id))
+						<!-- @include('site.common.ads', array('adPosition' => POSITION_NEWS_DETAIL_LEFT, 'model_name' => 'AdminNew', 'model_id' => $inputNew->id)) -->
 					@else
 						@include('site.common.ads', array('adPosition' => POSITION_MOBILE_NEWS_DETAIL_LEFT, 'model_name' => 'AdminNew', 'model_id' => $inputNew->id))
 					@endif
