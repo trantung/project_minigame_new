716e3da3f6aafc7d02be74bfae0b0fd4beb0ceff
dxnm <2711sq@gmail.com>
2016-04-14 14:50:51 +0700

    fix

--- app/views/site/common/ads.blade.php
+++ app/views/site/common/ads.blade.php
@@ -4,24 +4,29 @@
 	<?php $ads = AdCommon::getAd($adPosition, $model_name, $model_id); ?>
 @endif
 @if($ads)
-	@foreach($ads as $value)
+	@foreach($ads as $key => $value)
 		<?php 
 			if($model_name == 'AdminNew') {
 				$new = AdminNew::find($model_id);
+
 				if($new) {
 					if($new->sensitive = ACTIVE) {
-						$sensitive = ACTIVE;
+						$sensitive[$key] = ACTIVE;
 					} else {
-						$sensitive = INACTIVE;
+						// dd($new->toArray());
+						$sensitive[$key] = INACTIVE;
 					}
 				} else {
-					$sensitive = INACTIVE;
+					$sensitive[$key] = INACTIVE;
 				}
+			} else {
+				$sensitive[$key] = INACTIVE;
 			}
 		?>
-		@if($sensitive == ACTIVE)
+		@if($sensitive[$key] == ACTIVE)
 			<div class="clearfix center">{{ $value->adsense2 }}</div>
-		@else
+		@endif
+		@if($sensitive[$key] == INACTIVE)
 			<div class="clearfix center">{{ $value->adsense }}</div>
 		@endif
 	@endforeach
