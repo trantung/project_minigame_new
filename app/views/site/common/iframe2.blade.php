54d6f3581c959dba3a21a20150cde82cd03bff32
trantung <trantunghn196@gmail.com>
2016-04-15 13:33:11 +0700

    fix

--- app/views/site/common/iframe2.blade.php
+++ app/views/site/common/iframe2.blade.php
@@ -1,3 +1,4 @@
+
 <style>
 	body {
 		font-family: Arial;
@@ -65,39 +66,59 @@
 </style>
 <div class="kt-boxgame">
 	<div class="kt-boxgame-title">
-		<strong><a href="{{ url('/') }}" target="_blank">Game online</a></strong>
+		<strong><a href="http://game.kienthuc.net.vn" target="_blank">Game online</a></strong>
 	</div>
 	<div class="clearfix"></div>
 	<div class="kt-boxgame-content">
-		@if(!empty($dataList))
-			@foreach($dataList as $key => $value)
-				<?php $url = CommonGame::getUrlGame($value); ?>
-				<div class="kt-boxgame-item">
+													<div class="kt-boxgame-item">
 					<div class="kt-boxgame-right-images">
-						<a href="{{ $url }}" target="_blank">
-							<img src="{{ url(UPLOAD_GAME_AVATAR . '/' .  $value->image_url) }}" alt="{{ $value->name }}" />
+						<a href="http://game.kienthuc.net.vn/nau-an/banh-ngot-napoleon" target="_blank">
+							<img src="http://game.kienthuc.net.vn/images/game_avatar/banh-ngot-napoleon-icon-1460601372.jpg" alt="Bánh Ngọt Napoleon" />
 						</a>
 					</div>
 					<div class="kt-boxgame-right-text">
-						<a href="{{ $url }}" target="_blank">{{ $value->name }}</a>
+						<a href="http://game.kienthuc.net.vn/nau-an/banh-ngot-napoleon" target="_blank">Bánh Ngọt Napoleon</a>
 					</div>
 				</div>
-			@endforeach
-		@endif
-		@if(!empty($dataListGame))
-			@foreach($dataListGame as $key => $value)
-				<?php $url = CommonGame::getUrlGame($value); ?>
-				<div class="kt-boxgame-item">
+											<div class="kt-boxgame-item">
 					<div class="kt-boxgame-right-images">
-						<a href="{{ $url }}" target="_blank">
-							<img src="{{ url(UPLOAD_GAME_AVATAR . '/' .  $value->image_url) }}" alt="{{ $value->name }}" />
+						<a href="http://game.kienthuc.net.vn/chien-thuat/rap-xiec-quai-vat" target="_blank">
+							<img src="http://game.kienthuc.net.vn/images/game_avatar/rap-xiec-quai-vat-icon-1460537496.jpg" alt="Rạp xiếc quái vật" />
 						</a>
 					</div>
 					<div class="kt-boxgame-right-text">
-						<a href="{{ $url }}" target="_blank">{{ $value->name }}</a>
+						<a href="http://game.kienthuc.net.vn/chien-thuat/rap-xiec-quai-vat" target="_blank">Rạp xiếc quái vật</a>
 					</div>
 				</div>
-			@endforeach
-		@endif
-	</div>
+											<div class="kt-boxgame-item">
+					<div class="kt-boxgame-right-images">
+						<a href="http://game.kienthuc.net.vn/hanh-dong/tieu-diet-alien" target="_blank">
+							<img src="http://game.kienthuc.net.vn/images/game_avatar/Tieu-diet-Ailen-icon-1460356934.jpg" alt="Tiêu Diệt Alien" />
+						</a>
+					</div>
+					<div class="kt-boxgame-right-text">
+						<a href="http://game.kienthuc.net.vn/hanh-dong/tieu-diet-alien" target="_blank">Tiêu Diệt Alien</a>
+					</div>
+				</div>
+											<div class="kt-boxgame-item">
+					<div class="kt-boxgame-right-images">
+						<a href="http://game.kienthuc.net.vn/chien-thuat/contra-giai-cuu-the-gioi" target="_blank">
+							<img src="http://game.kienthuc.net.vn/images/game_avatar/contra-contra-icon-1459410103.jpg" alt="Contra - Giải cứu thế giới" />
+						</a>
+					</div>
+					<div class="kt-boxgame-right-text">
+						<a href="http://game.kienthuc.net.vn/chien-thuat/contra-giai-cuu-the-gioi" target="_blank">Contra - Giải cứu thế giới</a>
+					</div>
+				</div>
+																		<div class="kt-boxgame-item">
+					<div class="kt-boxgame-right-images">
+						<a href="http://game.kienthuc.net.vn/game-android/kungfu-warrior" target="_blank">
+							<img src="http://game.kienthuc.net.vn/images/game_avatar/kungfu-warrior-icon-1460450770.jpg" alt="Kungfu Warrior" />
+						</a>
+					</div>
+					<div class="kt-boxgame-right-text">
+						<a href="http://game.kienthuc.net.vn/game-android/kungfu-warrior" target="_blank">Kungfu Warrior</a>
+					</div>
+				</div>
+						</div>
 </div>
